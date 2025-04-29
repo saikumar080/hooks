7922437c858b50532091f5e184b95d572895145b
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2025-04-29 14:12:48 +0530

    useCallBack()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import NotUSeCallback from './UseCallback/NotUSeCallback';
 import Parent from './UseCallback/USecallback';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header>React Hooks</header>
       <Parent/>
+      <NotUSeCallback />
     </div>
   );
 }
--- src/UseCallback/NotUSeCallback.js
+++ src/UseCallback/NotUSeCallback.js
@@ -1,9 +1,28 @@
 import React,{useState} from 'react'
 
+// This component does not use useCallback, so it will re-render every time the parent re-renders
+const Child=React.memo(({onClick})=>{
+    console.log("child component render NotUSeCallback");
+    return(
+        <div>
+            <button onClick={onClick}>Click me</button>
+        </div>
+    )
+    
+})
+
 function NotUSeCallback() {
-  return (
-    <div>NotUSeCallback</div>
-  )
+    const[count,setCount]=useState(0);
+    const handleClick=()=>{
+        console.log("Button clicked not use callback")
+    }
+    return (
+        <>
+            <button onClick={()=>setCount(count+1)}>Parent count: {count}</button>
+            <Child onClick={handleClick}/>
+        </>
+    )
+ 
 }
 
 export default NotUSeCallback
